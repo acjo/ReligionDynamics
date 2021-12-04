a5abffc21493fb4979a31d974194199d7b983f7c
ojac <caelano@byu.edu>
2021-12-04 16:41:32 -0700

    adding bvp solver

--- religions.py
+++ religions.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import odeint, solve_ivp, solve_bvp
 
-def define_ivp_system(n, C):
+def define_ode_system(n, C):
     '''
     This returns a function defining the ODE system used in solve_ivp or odeint
     :param n: ( n, ) np.ndarray difference between birth rate and death rate of the ith relgion
@@ -27,60 +27,73 @@ def define_ivp_system(n, C):
         return np.array([P[i] * (n[i] + sum([(C[i, j] - C[j, i])*P[j]
                                              for j in range(size)]))
                                              for i in range(size)])
-
-
-
     return ode
 
-
-def numerical_solve(P0, t_span, M, n, C, method='ivp'):
+def numerical_solve(t_span, M, n, C, method='ivp', P0 = None, BC = None):
     '''
     Numerical solves the ivp using solve_ivp
     :parama P0: (n, ) np.ndarray array containing the initial value
     :param t_span: (tuple) contains the starting and ending time value
-    :param disc: ( int ) temporal discretization
+    :param M: ( int ) temporal discretization
     :param n: (n, ) np.ndarray difference between birth rate and death rate of the ith relgion
     :param C: (n, n) np.ndarray matrix containing conversion rate differences
-    :param I: (n, n) np.ndarray matrix containing interactions between religions
+    :param P0: (n, ) np.ndarray array containing the initial value, if method is 'bvp' then this is the initial guess
+    :param BC: (n, ) np.ndarray boundary conditions
     :param method ="ivp": boolean determing the method
     :return: callable the ode system
     '''
     if method == 'ivp':
-        ode = define_ivp_system(n, C)
+        ode = define_ode_system(n, C)
         t_eval = np.linspace(t_span[0], t_span[-1], M)
         sol =  solve_ivp(ode, t_span, P0, t_eval=t_eval)
-    elif method == 'odeint':
-        raise NotImplementedError('odeint not yet implemented')
-    elif method == 'solve_bvp':
-        raise NotImplementedError('solve_bvp not yet implemented')
+    elif method == 'bvp':
+        ode = define_ode_system(n, C)
+        t_eval = np.linspace(t_span[0], t_span[-1], M)
+        sol = solve_bvp(ode, BC, t_eval, P0 )
     else:
         raise ValueError('incorrect method')
 
     return sol
 
 
-def plot_sol(P0, t_span, M, n, C, method='ivp', labels=[]):
+def plot_sol(t_span, M, n, C, method='ivp', P0=None, BC=None, labels=[]):
     '''
     plots the solution
     '''
-    sol = numerical_solve(P0, t_span, M, n, C, method=method)
-    fig = plt.figure()
-    fig.set_dpi(300)
-    ax = fig.add_subplot(111)
-    for s, lab in zip(sol.y, labels):
-        ax.plot(sol.t, s, label=lab)
-        ax.legend(loc='best')
+    sol = numerical_solve(t_span, M, n, C, method=method, P0=P0, BC=BC)
+    if method == 'ivp':
+        fig = plt.figure()
+        fig.set_dpi(300)
+        ax = fig.add_subplot(111)
+        for s, lab in zip(sol.y, labels):
+            ax.plot(sol.t, s, label=lab)
+            ax.legend(loc='best')
 
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Population')
-    ax.set_title('SIR Model of Religions')
-    plt.show()
-    return None
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Population')
+        ax.set_title('SIR Model of Religions')
+        plt.show()
+
+    elif method == 'bvp':
+        fig = plt.figure()
+        fig.set_dpi(300)
+        ax = fig.add_subplot(111)
+        for s, lab in zip(sol.y, labels):
+            ax.plot(sol.x, s, label=lab)
+            ax.legend(loc='best')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Population')
+        ax.set_title('SIR Model of Religions')
+        plt.show()
+    else:
+        raise ValueError('Incorrect method')
+
+    return
 
 
 def BryceSection():
     P0, t_span, M, n, C, method, labels = np.array([10, 10, 10]), (0, 400),20000, np.array([.2, -0.02,-0.01]), np.array([[0, .001, .001], [.01, 0,.02],[.01,.02,0]]), 'ivp', ['No Conversion', 'A', 'B']
-    plot_sol(P0, t_span, M, n, C, method, labels)
+    plot_sol(t_span, M, n, C, method, P0= P0, labels=labels)
     #plot_sol([10,10], (0,10), 1, [0,0], np.array([[0,0.1],[0,0]]), method='ivp', labels=['A','B'])
 BryceSection()
 
